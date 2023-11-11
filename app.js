e53974216d19b27e06b3e9637dd618b6d55ab9d9
Jordan-Burgess <alfredjordan93@gmail.com>
2023-11-11 14:40:43 -0500

    Place win combos variable inside checkWin function

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let boxElems = document.querySelectorAll('.box')
-let turnElem = document.querySelector('.message')
-let scoreElems = document.querySelectorAll('.score')
-let newElem = document.querySelector('.new')
-let score = [0, 0]
-let markers = ['X', 'O']
+const boxElems = document.querySelectorAll('.box')
+const turnElem = document.querySelector('.message')
+const scoreElems = document.querySelectorAll('.score')
+const newElem = document.querySelector('.new')
+const score = [0, 0]
+const markers = ['X', 'O']
 let turn = 0
 let count = 0
 let active = true
-let winCombos = {
-    zero: [[0, 1, 2], [0, 4, 8], [0, 3, 6]],
-    one: [[0, 1, 2], [1, 4, 7]],
-    two: [[0, 1, 2], [2, 5, 8], [2, 4, 6]],
-    three: [[0, 3, 6], [3, 4, 5]],
-    four: [[0, 4, 8], [2, 4, 6], [1, 4, 7], [3, 4, 5]],
-    five: [[2, 5, 8], [3, 4, 5]],
-    six: [[0, 3, 6], [2, 4, 6], [6, 7, 8]],
-    seven: [[1, 4, 7], [6, 7, 8]],
-    eight: [[0, 4, 8], [6, 7, 8], [2, 5, 8]],
-}
 
-function checkWin(place, marker){
-    let wins = []
+const checkWin = function (place, marker){
+    const wins = []
+    const winCombos = {
+        zero: [[0, 1, 2], [0, 4, 8], [0, 3, 6]],
+        one: [[0, 1, 2], [1, 4, 7]],
+        two: [[0, 1, 2], [2, 5, 8], [2, 4, 6]],
+        three: [[0, 3, 6], [3, 4, 5]],
+        four: [[0, 4, 8], [2, 4, 6], [1, 4, 7], [3, 4, 5]],
+        five: [[2, 5, 8], [3, 4, 5]],
+        six: [[0, 3, 6], [2, 4, 6], [6, 7, 8]],
+        seven: [[1, 4, 7], [6, 7, 8]],
+        eight: [[0, 4, 8], [6, 7, 8], [2, 5, 8]],
+    }
     for(let i=0; i<winCombos[place].length; i++){
         for(let j=0; j<winCombos[place][i].length; j++){
             if(boxElems[winCombos[place][i][j]].innerText != marker){
